570997262480456a0673dc006a91a4726cbfe599
Tanjin He <tanjin_he@berkeley.edu>
2021-11-03 13:06:31 -0700

    update with lastest transformers (4.11.3)

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ __email__ = 'tanjin_he@berkeley.edu, rongzq08@gmail.com'
 
 if __name__ == "__main__":
     setup(name='materials_entity_recognition',
-          version=3.1,
+          version=3.2,
           author="Tanjin He",
           author_email="tanjin_he@berkeley.edu",
           license="MIT License",
           packages=find_packages(),
           include_package_data=True,
           install_requires=[
-              'tensorflow>=2.3.0',
-              'tensorflow-addons>=0.12.0',
+              'tensorflow>=2.4.0',
+              'tensorflow-addons>=0.12.1',
               'spacy',
               'chemdataextractor',
               'numpy',
-              'transformers>=4.3.2',
+              'transformers>=4.4.2',
               'torch',
               'psutil',
           ],
